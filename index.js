c7950e912da17b688034c1d5cc840faf01ada52e
Shahzaib Malik <72553469+shahzaibb101@users.noreply.github.com>
2025-05-12 18:45:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["https://project-estimation-tool-frontend.vercel.app/", "http://localhost:3000"],
+    origin: ["https://project-estimation-tool-frontend.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "OPTIONS", "PUT"],
     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
